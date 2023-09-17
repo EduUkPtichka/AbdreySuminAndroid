50b9b832f974b966b052af891a0dff588d718648
EduUkPtichka <ukenoveldiyar@gmail.com>
2023-09-17 17:15:20 +0700

    Initial commit

---
+++ app/moduleone/src/main/java/com/example/moduleone/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.example.moduleone.data
+
+import com.example.moduleone.domain.ShopItem
+import com.example.moduleone.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId
+            autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Null ")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+
+}
+
